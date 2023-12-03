68818fb1114c710911640bf4779be96b306f1f93
Qwerty000101 <andreyiv20005@gmail.com>
2023-12-03 21:41:35 +0300

    Final_Fix_0.04

--- Alg_lr5_1/Alg_lr5_1.py
+++ Alg_lr5_1/Alg_lr5_1.py
@@ -1,4 +1,8 @@
-﻿from random import randint
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from random import randint
 import random
 import numpy as np
 import timeit
@@ -39,65 +43,69 @@ def correlation(array_of_values_x,array_of_values_y):
     return pair_correlation_coefficient
 
 
-time_min = []
-x = []
-for i in range(1,101):
-    arr = [0 for i in range(0,i)]
-    x.append(i)
-    for j in range(0,len(arr)):
-        arr[j] = random.randint(500,1000)
-    search_time = (timeit.timeit(lambda: bubble_sort(arr), number=50))/50
-    print("Время сортировки массива из ",i," элементов: ",search_time,"\n")
-    print(arr,"\n")
-    time_min.append(search_time)
-sum_y = sum(time_min)
-sum_x = sum(x)
-sum_x2 = 0
-sum_x3 = 0
-sum_x4 = 0
-sum_yx2 = 0
-sum_yx = 0
+if __name__ == '__main__':
+    time_min = []
+    x = []
 
-for i in range(0,len(x)):
-    sum_x2 += i*i
-    sum_x3 += i*i*i
-    sum_x4 += i*i*i*i
-    sum_yx2 += time_min[i]*i*i
-    sum_yx += time_min[i]*i
+    for i in range(1,101):
+        arr = [0 for i in range(0,i)]
+        x.append(i)
 
-an = len(x)
-matrix = np.array([[sum_x4, sum_x3,sum_x2], 
-                   [sum_x3, sum_x2, sum_x], 
-                   [sum_x2, sum_x, an]])
-det = np.linalg.det(matrix)
-matrix_x = np.array([[sum_x4, sum_x3,sum_yx2], 
-                     [sum_x3, sum_x2, sum_yx], 
-                     [sum_x2, sum_x, sum_y]])
-first_det = np.linalg.det(matrix_x)
-matrix_y = np.array([[sum_x4, sum_yx2,sum_x2], 
-                     [sum_x3, sum_yx, sum_x], 
-                     [sum_x2, sum_y, an]])
-second_det = np.linalg.det(matrix_y)
-matrix_z = np.array([[sum_yx2, sum_x3,sum_x2], 
-                     [sum_yx, sum_x2, sum_x], 
-                     [sum_y, sum_x, an]])
-third_det = np.linalg.det(matrix_z)
-a = third_det/det
-b = second_det/det
-c = first_det/det
-func = []
+        for j in range(0,len(arr)):
+            arr[j] = random.randint(500,1000)
 
-for i in x:
-    func.append(a*i*i+b*i+c)
+        search_time = (timeit.timeit(lambda: bubble_sort(arr), number=50))/50
+        print("Время сортировки массива из ",i," элементов: ",search_time,"\n")
+        print(arr,"\n")
+        time_min.append(search_time)
 
-plt.figure(figsize=(10,6))
-plt.figure(1)
-plt.title("Зависимость времени сортировки от размера массива")
-plt.plot(x,func,color="red",linewidth=4)
-plt.scatter(x, time_min,s=3)
-plt.xlabel("Размер массива\n Коэффициент парной корреляции равен:\
-"+str(correlation(x,time_min)))
-plt.legend(["y="+str(a)+"*x^2+("+str(b)+")*x"+str(c)])
-plt.ylabel("Время сортировки массива")
-plt.show()
+    sum_y = sum(time_min)
+    sum_x = sum(x)
+    sum_x2 = 0
+    sum_x3 = 0
+    sum_x4 = 0
+    sum_yx2 = 0
+    sum_yx = 0
 
+    for i in range(0,len(x)):
+        sum_x2 += i*i
+        sum_x3 += i*i*i
+        sum_x4 += i*i*i*i
+        sum_yx2 += time_min[i]*i*i
+        sum_yx += time_min[i]*i
+
+    an = len(x)
+    matrix = np.array([[sum_x4, sum_x3,sum_x2], 
+                       [sum_x3, sum_x2, sum_x], 
+                       [sum_x2, sum_x, an]])
+    det = np.linalg.det(matrix)
+    matrix_x = np.array([[sum_x4, sum_x3,sum_yx2], 
+                         [sum_x3, sum_x2, sum_yx], 
+                         [sum_x2, sum_x, sum_y]])
+    first_det = np.linalg.det(matrix_x)
+    matrix_y = np.array([[sum_x4, sum_yx2,sum_x2], 
+                         [sum_x3, sum_yx, sum_x], 
+                         [sum_x2, sum_y, an]])
+    second_det = np.linalg.det(matrix_y)
+    matrix_z = np.array([[sum_yx2, sum_x3,sum_x2], 
+                         [sum_yx, sum_x2, sum_x], 
+                         [sum_y, sum_x, an]])
+    third_det = np.linalg.det(matrix_z)
+    a = third_det/det
+    b = second_det/det
+    c = first_det/det
+    func = []
+
+    for i in x:
+        func.append(a*i*i+b*i+c)
+
+    plt.figure(figsize=(10,6))
+    plt.figure(1)
+    plt.title("Зависимость времени сортировки от размера массива")
+    plt.plot(x,func,color="red",linewidth=4)
+    plt.scatter(x, time_min,s=3)
+    plt.xlabel("Размер массива\n Коэффициент парной корреляции равен:\
+               "+str(correlation(x,time_min)))
+    plt.legend(["y="+str(a)+"*x^2+("+str(b)+")*x"+str(c)])
+    plt.ylabel("Время сортировки массива")
+    plt.show()
